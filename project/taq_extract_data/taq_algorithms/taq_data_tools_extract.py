fb36eb5272193798890e5156676fc44d2a41d895
juanhenao21 <juanhenao21@gmail.com>
2020-03-13 16:16:44 +0100

    Update ticker info function

--- project/taq_extract_data/taq_algorithms/taq_data_tools_extract.py
+++ project/taq_extract_data/taq_algorithms/taq_data_tools_extract.py
@@ -249,14 +249,14 @@ def taq_get_tickers_data(year):
     abs_path = os.path.abspath(__file__).split('/')
     # Take the path from the start to the project folder
     root_path = '/'.join(abs_path[:abs_path.index('project') + 1])
-    f_path = root_path + f'/taq_data/original_year_data_{year}'
+    f_path = root_path + f'/taq_data/hdf5_daily_data_{year}'
     files = os.listdir(f_path)
 
     tickers = []
 
     # Get the ticker symbols
     for file in files:
-        tickers.append(file.split('_')[0])
+        tickers.append(file.split('_')[1])
 
     tickers = list(set(tickers))
 
