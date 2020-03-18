1900cf696730caf76e676538fb9248ed674f5f99
juanhenao21 <juanhenao21@gmail.com>
2020-03-18 07:36:33 +0100

    Update to run in server

--- project/taq_extract_data/taq_algorithms/taq_data_main_extract.py
+++ project/taq_extract_data/taq_algorithms/taq_data_main_extract.py
@@ -50,9 +50,9 @@ def taq_data_plot_generator(tickers, year):
     with mp.Pool(processes=mp.cpu_count()) as pool:
 
         # Basic functions
-        pool.starmap(taq_data_analysis_extract
-                     .taq_midpoint_physical_data,
-                     iprod(tickers, date_list))
+        # pool.starmap(taq_data_analysis_extract
+        #              .taq_midpoint_physical_data,
+        #              iprod(tickers, date_list))
         pool.starmap(taq_data_analysis_extract
                      .taq_trade_signs_physical_data,
                      iprod(tickers, date_list))
