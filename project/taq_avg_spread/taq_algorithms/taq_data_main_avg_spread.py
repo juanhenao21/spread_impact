a2d7201d7bcdca034f53a9002c6f4417c8119dbc
juanhenao21 <juanhenao21@gmail.com>
2020-03-24 09:10:47 +0100

    Check tickers

--- project/taq_avg_spread/taq_algorithms/taq_data_main_avg_spread.py
+++ project/taq_avg_spread/taq_algorithms/taq_data_main_avg_spread.py
@@ -64,7 +64,8 @@ def main():
     # To be used when run in server
     year = '2008'
     tickers = taq_data_tools_avg_spread.taq_get_tickers_data(year)
-    print(tickers.sort())
+    tickers.sort()
+    print(tickers)
     print(len(tickers))
 
     # Basic folders
